d3e0a7468c7f5e13f2bf32bb4ddf42dfbb5430ef
k1mhyewon <kimhy0434@gmail.com>
2022-09-14 16:26:50 +0900

    혜원 테스트 커밋 3

--- Groupware_1jo/src/main/java/common/controller/CommonCtrl.java
+++ Groupware_1jo/src/main/java/common/controller/CommonCtrl.java
@@ -3,7 +3,7 @@ package common.controller;
 public class CommonCtrl {
 
 	public static void main(String[] args) {
-		System.out.println("다같이 사용하는 클래스");
+		System.out.println("다같이 사용하는 클래스임");
 
 	}
 
